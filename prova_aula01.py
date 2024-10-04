692c2c6dcb3e94b28b0de08d09aae1b4b97e4072
New Dev <161941955+NewDevYHWH@users.noreply.github.com>
2024-10-03 21:01:44 -0300

    Add files via upload

---
+++ prova_aula01.py
@@ -0,0 +1,8 @@
+print('Resposta prova LPIA-A01')
+
+base = int(input('Qual o valor da base do retangulo? '))
+altura = int(input('Qual o valor da altura do retangulo? '))
+
+area = base * altura
+
+print(f'A área do retangulo é de: {area}')
