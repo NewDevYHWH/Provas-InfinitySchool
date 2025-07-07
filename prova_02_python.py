3b47f742ab17db90f885382dc9f5fb23697ca3ca
New Dev <161941955+NewDevYHWH@users.noreply.github.com>
2025-07-07 18:52:50 -0300

    Add files via upload

---
+++ prova_02_python.py
@@ -0,0 +1,17 @@
+#PYIA-A02] Crie um dicionário que irá armazenar informações de um contato, incluindo o nome, o telefone e o email.
+# Peça ao usuário para fornecer esses dados, solicitando que ele insira o nome do contato,
+# o número de telefone e o endereço de email. Após coletar todas as informações necessárias,
+# exiba o conteúdo do dicionário, mostrando todas as informações do contato inserido pelo usuário.
+
+nome = input("Digite o nome do contato: ")
+telefone = input("Digite o número de telefone do contato: ")
+email = input("Digite o e-mail do contato: ")
+
+contato = {
+    "nome": nome,
+    "telefone": telefone,
+    "email": email
+}
+print("\nInformações do contato inserido:")
+for chave, valor in contato.items():
+    print(f"{chave.capitalize()}: {valor}")
