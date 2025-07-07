eaeeb8669ae6f0cc46bbbfc974ce316c589082de
New Dev <161941955+NewDevYHWH@users.noreply.github.com>
2025-07-07 18:45:53 -0300

    Add files via upload

---
+++ prova_01_python.py
@@ -0,0 +1,9 @@
+#[PYIA-A01] Crie uma lista contendo seis frutas de sua escolha. Depois de ter a lista pronta,
+#converta essa lista em uma tupla. Por fim, exiba o conteúdo da tupla resultante para verificar as
+#frutas que foram armazenadas.
+
+frutas_lista = ["maçã", "banana", "laranja", "uva", "abacaxi", "melancia"]
+
+frutas_tupla = tuple(frutas_lista)
+
+print("Frutas armazenadas na tupla:", frutas_tupla)
