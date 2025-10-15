99d20af24ea6823a26a5d2e1b9a77bbee980fd05
New Dev <161941955+NewDevYHWH@users.noreply.github.com>
2025-10-15 12:06:22 -0300

    Add files via upload

---
+++ prova_12_python.py
@@ -0,0 +1,24 @@
+class Veiculo:
+    def movimentar(self):
+        print("Veículo está em movimento.")
+
+class Carro(Veiculo):
+    def movimentar(self):
+        print("Carro está dirigindo.")
+
+class Moto(Veiculo):
+    def movimentar(self):
+        print("Moto está acelerando.")
+        
+def main():
+    veiculo = Veiculo()
+    veiculo.movimentar()
+    
+    carro = Carro()
+    carro.movimentar()
+
+    moto = Moto()
+    moto.movimentar()
+    
+if __name__ == "__main__":
+    main()
